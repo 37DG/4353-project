3676431648775a6195de5140e625065cad1c9cff
jjjzhu-5847 <66804994+jjjzhu-5847@users.noreply.github.com>
2025-02-08 15:14:13 -0600

    Update urls.py

--- software design project/project/UserManagement/urls.py
+++ software design project/project/UserManagement/urls.py
@@ -4,13 +4,23 @@ from UserManagement import views
 app_name = 'UserManagement'
 
 urlpatterns = [
+    # index URL
     path('', views.index, name='index'),
 
+    # dashboard URL
     path('dashboard/', views.dashboard, name='dashboard'),
 
+    # fetch user when user try to login from dashboard
     path("check-user/", views.check_user, name="check_user"),
 
+    # Administrator URL
     path('Administrator/', views.Administrator, name='Administrator'),
 
-    path('Basicuser', views.Basicuser, name='Basicuser')
+    # Basicuser URL
+    path('Basicuser/', views.Basicuser, name='Basicuser'),
+
+    # create, update, delete user URL to fetch data from Administrator page
+    path('create-user/', views.create_user, name='create_user'),
+    path('update-user/', views.update_user, name='update_user'),
+    path('delete-user/', views.delete_user, name='delete_user'),
 ]

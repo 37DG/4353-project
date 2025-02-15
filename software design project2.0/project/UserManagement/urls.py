0d3344551fa1dfe92c0562036de7039a26ed95ef
jjjzhu-5847 <66804994+jjjzhu-5847@users.noreply.github.com>
2025-02-15 15:51:09 -0600

    Add files via upload

---
+++ software design project2.0/project/UserManagement/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from UserManagement import views
+
+app_name = 'UserManagement'
+
+urlpatterns = [
+    # index URL
+    path('', views.index, name='index'),
+    
+    path('auth/login/', views.microsoft_login, name='microsoft_login'),  # Redirect to Microsoft
+    path('auth/logout/', views.logout_view, name='logout'),  # Logout URL
+    path('auth/callback/', views.login_view, name='auth_callback'),
+
+    # Deactivate user URL
+    path('Deactivated_User/', views.Deactivated_User, name='Deactivated_User'),
+
+    # Administrator URL
+    path('Administrator/', views.Administrator, name='Administrator'),
+
+    # Basicuser URL
+    path('Basicuser/', views.Basicuser, name='Basicuser'),
+
+    # create, update, delete user URL to fetch data from Administrator page
+    path('create-user/', views.create_user, name='create_user'),
+    path('update-user/', views.update_user, name='update_user'),
+    path('delete-user/', views.delete_user, name='delete_user'),
+]

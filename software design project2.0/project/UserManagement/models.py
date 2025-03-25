c3350d5a80e0912d686cbc265a4625cf5406ba01
jjjzhu-5847 <66804994+jjjzhu-5847@users.noreply.github.com>
2025-03-24 19:40:47 -0500

    Delete software design project2.0 directory

--- software design project2.0/project/UserManagement/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    email = models.CharField(max_length=255, primary_key=True)
-    name = models.CharField(max_length=255, null=False)
-    role = models.CharField(max_length=45, null=False)
-    status = models.CharField(max_length=1, null=False)
